11756c9094f62353e08d9da5a5c0aa5953e7aaa5
Nikita Gulyaev <mikita.glv@gmail.com>
2021-01-28 20:52:29 +0300

    Refactoring

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,19 +4,19 @@ from random import randint
 GAME_DESCRIPTION = "What number is missing in the progression?"
 
 
-def make_progression():
+def make_progression(start, step, length):
     progression = []
-    start_number = randint(1, 100)
-    step = randint(1, 10)
-    length = 10
     while len(progression) < length:
-        progression.append(start_number)
-        start_number += step
+        progression.append(start)
+        start += step
     return progression
 
 
 def game_question():
-    progression = make_progression()
+    start = randint(1, 100)
+    step = randint(1, 10)
+    length = 10
+    progression = make_progression(start, step, length)
     question_progression = []
     question_progression.extend(progression)
     hiden_index = randint(0, 9)
