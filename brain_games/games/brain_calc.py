0e5ded4957c0399043feacbea8904a6ff2e7c0d1
Nikita Gulyaev <mikita.glv@gmail.com>
2021-01-28 20:29:46 +0300

    Refactoring

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,20 +1,29 @@
-from random import randint
+from random import randint, choice
+import operator
 
 
-GAME_DESCRIPTION = "What is the resul of the expression?"
+GAME_DESCRIPTION = "What is the result of the expression?"
+
+
+operations = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+}
+
+
+def random_operation():
+    return choice(list(operations.keys()))
+
+
+def correct_answer(first_number, second_number, operation):
+    return str(operations[operation](first_number, second_number))
 
 
 def game_question():
     first_number = randint(1, 100)
     second_number = randint(1, 100)
-    random_calculation = randint(1, 3)
-    if random_calculation == 1:
-        answer = first_number + second_number
-        question = f"Question: {first_number} + {second_number}"
-    elif random_calculation == 2:
-        answer = first_number - second_number
-        question = f"Question: {first_number} - {second_number}"
-    elif random_calculation == 3:
-        answer = first_number * second_number
-        question = f"Question: {first_number} * {second_number}"
+    operation = random_operation()
+    question = f"Question: {first_number} {operation} {second_number}"
+    answer = correct_answer(first_number, second_number, operation)
     return (question, answer)
