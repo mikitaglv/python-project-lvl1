392b059139e7170a47d8c7f1872bd310bba1da3b
Nikita Gulyaev <mikita.glv@gmail.com>
2021-02-09 14:15:10 +0300

    Fixed linter errors

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from brain_games import cli
 
+
 def main():
     print("Welcome to the Brain Games!")
     user_name = cli.ask_question("May I have your name? ")
     print(f"Hello, {user_name}!")
+
 
 if __name__ == "__main__":
     main()
