bffd540438fe1b5ed20c863f2428734cdfcc3d86
Nikita Gulyaev <mikita.glv@gmail.com>
2021-02-09 12:28:05 +0300

    Fixed is_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,17 +5,19 @@ GAME_DESCRIPTION = 'Answer "yes" if the number is prime, otherwise answer "no".'
 
 
 def is_prime(num):
+    if num <= 1:
+        return False
     for i in range(2, num):
         if num % i == 0 and i < num:
-            return True
-    return False
+            return False
+    return True
 
 
 def game_question():
     number = randint(1, 100)
     question = f"Question: {number}"
     if is_prime(number):
-        correct_answer = "no"
-    else:
         correct_answer = "yes"
+    else:
+        correct_answer = "no"
     return (question, correct_answer)
